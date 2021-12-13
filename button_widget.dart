6a1d1114e7fded4c4ba5f0a76fe06c159af9bded
Zeynep-Parlatan <92530854+Zeynep-Parlatan@users.noreply.github.com>
2021-12-13 17:55:33 +0300

    Add files via upload

---
+++ button_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:version8_timer/timepage.dart';
+class ButtonWidget extends StatelessWidget{
+  final String text;
+  final Color backgroundColor;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    this.backgroundColor=Colors.blue,
+    required this.onClicked,
+    }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: backgroundColor,
+            padding: EdgeInsets.symmetric(horizontal: 32,vertical: 16)
+          ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20),
+        ),
+
+      onPressed:
+        //Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>Clockpage()));
+        onClicked,
+
+
+
+
+
+
+    );
+    }
+  }
